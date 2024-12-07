b6135a44703f6dafd6189ae3dca3f4a487c3b059
Ian Haggerty <ianhaggerty3@gmail.com>
2024-12-07 13:34:00 -0500

    Solve day 6 part 2

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -104,7 +104,7 @@ public class Day06 : BaseDay
         int height;
         var width = 0;
         HashSet<(int, int)> locations = new();
-        Dictionary<(int, int), HashSet<Direction>> visited = new();
+        Dictionary<(int, int), Direction> visited = new();
         foreach (var line in new LineReader(() => new StringReader(_input)))
         {
             for (var j = 0; j < line.Length; j++)
@@ -128,182 +128,58 @@ public class Day06 : BaseDay
 
         var currentPosition = (iStart, jStart);
         var currentDirection = Direction.Up;
-        var isStart = true;
-        while (IsInRange(currentPosition) && !visited.GetValueOrDefault(currentPosition, new HashSet<Direction>())
-                   .Contains(currentDirection))
+        while (IsInRange(currentPosition))
         {
-            if (isStart)
-            {
-                isStart = false;
-            }
-            else
-            {
-                var directionHistory = visited.GetValueOrDefault(currentPosition, new HashSet<Direction>());
-                directionHistory.Add(currentDirection);
-                visited[currentPosition] = directionHistory;
-            }
+            visited.TryAdd(currentPosition, currentDirection);
 
-            var nextState = GetNextState(currentPosition, currentDirection);
+            var nextState = GetNextState(currentPosition, currentDirection, locations);
             currentPosition = nextState.Item1;
             currentDirection = nextState.Item2;
         }
 
         var loopPositions = new HashSet<(int, int)>();
-        foreach (var state in visited)
-        foreach (var direction in state.Value)
-            if (IsPotentialLoop(state.Key, direction))
+        foreach (var state in visited.Where(p => p.Key != (iStart, jStart)))
+        {
+            var obstructions = locations.Concat([state.Key]).ToHashSet();
+            var reverseDiff = GetReversePositionDiff(state.Value);
+            var start = (state.Key.Item1 + reverseDiff.Item1, state.Key.Item2 + reverseDiff.Item2);
+            if (SimulationEndInLoop(obstructions, start, state.Value))
                 loopPositions.Add(state.Key);
+        }
 
 
         return new ValueTask<string>($"{loopPositions.Count}");
 
-        bool IsPotentialLoop((int, int) position, Direction direction)
+        bool SimulationEndInLoop(HashSet<(int, int)> obstructions, (int, int) startingPosition,
+            Direction startingDirection)
         {
-            if (direction == Direction.Up)
+            var currentPosition = startingPosition;
+            var currentDirection = startingDirection;
+            Dictionary<(int, int), HashSet<Direction>> visited = new();
+
+            while (IsInRange(currentPosition))
             {
-                var topLeft = (position.Item1 + 1, position.Item2);
-                var topRightObstruction = GetNearestLocationToRight(topLeft);
-                if (topRightObstruction is null)
-                    return false;
-                var topRight = (topRightObstruction.Value.Item1, topRightObstruction.Value.Item2 - 1);
+                if (visited.GetValueOrDefault(currentPosition, new HashSet<Direction>())
+                    .Contains(currentDirection))
+                    return true;
 
-                var bottomRightObstruction = GetNearestLocationBelow(topRight);
+                var directionHistory = visited.GetValueOrDefault(currentPosition, new HashSet<Direction>());
+                directionHistory.Add(currentDirection);
+                visited[currentPosition] = directionHistory;
 
-                if (bottomRightObstruction is null)
-                    return false;
-                var bottomRight = (bottomRightObstruction.Value.Item1 - 1, bottomRightObstruction.Value.Item2);
-
-                var bottomLeftObstruction = GetNearestLocationToLeft(bottomRight);
-                if (bottomLeftObstruction is null)
-                    return false;
-
-                var bottomLeft = (bottomLeftObstruction.Value.Item1, bottomLeftObstruction.Value.Item2 + 1);
-                var topLeftObstruction = GetNearestLocationAbove(bottomLeft);
-                if (topLeftObstruction is not null && topLeftObstruction.Value.Item1 > position.Item1)
-                    return false;
-                
-                Console.WriteLine("Reporting true for up");
-            }
-            else if (direction == Direction.Down)
-            {
-                var bottomRight = (position.Item1 - 1, position.Item2);
-                
-                var bottomLeftObstruction = GetNearestLocationToLeft(bottomRight);
-                if (bottomLeftObstruction is null)
-                    return false;
-                
-                var bottomLeft = (bottomLeftObstruction.Value.Item1, bottomLeftObstruction.Value.Item2 + 1);
-                var topLeftObstruction = GetNearestLocationAbove(bottomLeft);
-                if (topLeftObstruction is null )
-                    return false;
-                
-                var topLeft = (topLeftObstruction.Value.Item1 + 1, topLeftObstruction.Value.Item2);
-                var topRightObstruction = GetNearestLocationToRight(topLeft);
-                if (topRightObstruction is null)
-                    return false;
-                var topRight = (topRightObstruction.Value.Item1, topRightObstruction.Value.Item2 - 1);
-
-                var bottomRightObstruction = GetNearestLocationBelow(topRight);
-
-                if (bottomRightObstruction is not null && bottomRightObstruction.Value.Item1 < position.Item1)
-                    return false;
-                
-                Console.WriteLine("Reporting true for down");
-            }
-            else if (direction == Direction.Left)
-            {
-                var bottomLeft = (position.Item1, position.Item2 + 1);
-                
-                var topLeftObstruction = GetNearestLocationAbove(bottomLeft);
-                if (topLeftObstruction is null )
-                    return false;
-                
-                var topLeft = (topLeftObstruction.Value.Item1 + 1, topLeftObstruction.Value.Item2);
-                var topRightObstruction = GetNearestLocationToRight(topLeft);
-                if (topRightObstruction is null)
-                    return false;
-                var topRight = (topRightObstruction.Value.Item1, topRightObstruction.Value.Item2 - 1);
-
-                var bottomRightObstruction = GetNearestLocationBelow(topRight);
-
-                if (bottomRightObstruction is null)
-                    return false;
-                
-                var bottomRight = (bottomRightObstruction.Value.Item1 - 1, bottomRightObstruction.Value.Item2);
-                
-                var bottomLeftObstruction = GetNearestLocationToLeft(bottomRight);
-                if (bottomLeftObstruction is not null && bottomLeftObstruction.Value.Item2 > position.Item2)
-                    return false;
-                
-                Console.WriteLine("Reporting true for left");
-            }
-            else if (direction == Direction.Right)
-            {
-                var topRight = (position.Item1, position.Item2 - 1);
-                
-                var bottomRightObstruction = GetNearestLocationBelow(topRight);
-                if (bottomRightObstruction is null)
-                    return false;
-                
-                var bottomRight = (bottomRightObstruction.Value.Item1 - 1, bottomRightObstruction.Value.Item2);
-                var bottomLeftObstruction = GetNearestLocationToLeft(bottomRight);
-                if (bottomLeftObstruction is null)
-                    return false;
-                
-                var bottomLeft = (bottomLeftObstruction.Value.Item1, bottomLeftObstruction.Value.Item2 + 1);
-                var topLeftObstruction = GetNearestLocationAbove(bottomLeft);
-                if (topLeftObstruction is null )
-                    return false;
-                
-                var topLeft = (topLeftObstruction.Value.Item1 + 1, topLeftObstruction.Value.Item2);
-                var topRightObstruction = GetNearestLocationToRight(topLeft);
-                if (topRightObstruction is not null && topRightObstruction.Value.Item2 < position.Item2)
-                    return false;
-                
-                Console.WriteLine("Reporting true for right");
+                var nextState = GetNextState(currentPosition, currentDirection, obstructions);
+                currentPosition = nextState.Item1;
+                currentDirection = nextState.Item2;
             }
 
-            return true;
+            return false;
         }
 
-        (int, int)? GetNearestLocationAbove((int, int) position)
-        {
-            return locations
-                .Where(l => l.Item2 == position.Item2 && l.Item1 < position.Item1)
-                .OrderBy(l => l.Item1).LastOrDefault();
-        }
-
-        (int, int)? GetNearestLocationBelow((int, int) position)
-        {
-            return locations
-                .Where(l => l.Item2 == position.Item2 && l.Item1 > position.Item1)
-                .OrderBy(l => l.Item1).FirstOrDefault();
-        }
-
-        (int, int)? GetNearestLocationToRight((int, int) position)
-        {
-            return locations
-                .Where(l => l.Item1 == position.Item1 && l.Item2 > position.Item2)
-                .OrderBy(l => l.Item2).FirstOrDefault();
-        }
-
-        (int, int)? GetNearestLocationToLeft((int, int) position)
-        {
-            return locations.Where(l => l.Item1 == position.Item1 && l.Item2 < position.Item2)
-                .OrderBy(l => l.Item2).LastOrDefault();
-        }
-
-        // break into functions 
-        // make sure other three sides are the same
-
-        // GetLoopWidth(D)
-
-
-        ((int, int), Direction) GetNextState((int, int) current, Direction direction)
+        ((int, int), Direction) GetNextState((int, int) current, Direction direction, HashSet<(int, int)> obstructions)
         {
             var diff = GetPositionDiff(direction);
             var next = (current.Item1 + diff.Item1, current.Item2 + diff.Item2);
-            if (locations.Contains(next)) return (current, GetNextDirection(direction));
+            if (obstructions.Contains(next)) return (current, GetNextDirection(direction));
 
             return (next, direction);
         }
