1a8c1d9f2d9c5c7675a39b986c580add3a56d525
Ian Haggerty <ianhaggerty3@gmail.com>
2024-12-10 21:34:53 -0500

    Add day 10 example

---
+++ AdventOfCode/Day10.cs
@@ -0,0 +1,29 @@
+﻿using MiscUtil.IO;
+
+namespace AdventOfCode;
+
+public class Day10 : BaseDay
+{
+    private readonly string _input;
+
+    public Day10()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        List<string> lines = new();
+        foreach (var line in new LineReader(() => new StringReader(_input)))
+        {
+            lines.Add(line);
+        }
+        
+        return new ValueTask<string>($"{0}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>($"{0}");
+    }
+}
