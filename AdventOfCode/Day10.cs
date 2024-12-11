b2f1027cfa12bdb0b3c2024ea14d7f77f4ccab14
Ian Haggerty <ianhaggerty3@gmail.com>
2024-12-10 22:06:35 -0500

    Solve day 10

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -13,17 +13,111 @@ public class Day10 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        List<string> lines = new();
+        Dictionary<(int, int), int> map = new();
+        int i = 0;
         foreach (var line in new LineReader(() => new StringReader(_input)))
         {
-            lines.Add(line);
+            int j = 0;
+            foreach (var token in line)
+            {
+                map[(i, j)] = int.Parse([token]);
+                j++;
+            }
+
+            i++;
+        }
+
+        var startingPositions = map.Where(p => p.Value == 0);
+
+        var total = 0;
+        foreach (var startingPosition in startingPositions) total += GetScore(startingPosition.Key).Count;
+
+        HashSet<(int, int)> GetScore((int, int) position)
+        {
+            var currentPosition = position;
+            while (true)
+            {
+                var currentValue = map[currentPosition];
+
+                if (currentValue == 9)
+                    return [currentPosition];
+
+                var next = currentValue + 1;
+                var nextPositions = map.Where(p => p.Value == next && Diff(p.Key, currentPosition) == 1).ToList();
+
+                if (nextPositions.Count == 0) return [];
+                if (nextPositions.Count == 1)
+                    currentPosition = nextPositions[0].Key;
+                else if (nextPositions.Count > 1)
+                    return nextPositions.Select(pos => GetScore(pos.Key)).SelectMany(p => p).ToHashSet();
+            }
+        }
+
+        int Diff((int, int) a, (int, int) b)
+        {
+            return Math.Abs(a.Item1 - b.Item1) + Math.Abs(a.Item2 - b.Item2);
         }
         
-        return new ValueTask<string>($"{0}");
+        return new ValueTask<string>($"{total}");
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return new ValueTask<string>($"{0}");
+        Dictionary<(int, int), int> map = new();
+        int i = 0;
+        foreach (var line in new LineReader(() => new StringReader(_input)))
+        {
+            int j = 0;
+            foreach (var token in line)
+            {
+                map[(i, j)] = int.Parse([token]);
+                j++;
+            }
+
+            i++;
+        }
+
+        var startingPositions = map.Where(p => p.Value == 0);
+
+        var total = 0;
+        foreach (var startingPosition in startingPositions)
+        {
+            total += GetScore(startingPosition.Key);
+        }
+
+        int GetScore((int, int) position)
+        {
+            var currentPosition = position;
+            while (true)
+            {
+                var currentValue = map[currentPosition];
+
+                if (currentValue == 9)
+                    return 1;
+                
+                var next = currentValue + 1;
+                var nextPositions = map.Where(p => p.Value == next && (Diff(p.Key, currentPosition) == 1)).ToList();
+
+                if (nextPositions.Count == 0)
+                {
+                    return 0;
+                }
+                if (nextPositions.Count == 1)
+                {
+                    currentPosition = nextPositions[0].Key;
+                }
+                else if (nextPositions.Count > 1)
+                {
+                    return nextPositions.Sum(pos => GetScore(pos.Key));
+                }
+            }
+        }
+
+        int Diff((int, int) a, (int, int) b)
+        {
+            return Math.Abs(a.Item1 - b.Item1) + Math.Abs(a.Item2 - b.Item2);
+        }
+        
+        return new ValueTask<string>($"{total}");
     }
 }
